0d624b7e3a5d39153846bf80d4ce851dc9882b3d
Henrique Mauri <hgmauri@gmail.com>
2021-08-29 14:41:39 -0300

    initial

---
+++ src/Sample.DotNet6.Api/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Sample.DotNet6.Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddCors();
+
+builder.Services.AddScoped<IHelloService, HelloService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sample API .NET 6.0", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
+
+app.UseCors(p =>
+{
+    p.AllowAnyOrigin();
+    p.WithMethods("GET");
+    p.AllowAnyHeader();
+});
+
+app.MapGet("/example", (bool? isHappy, IHelloService service, ClaimsPrincipal user) =>
+{
+    if (isHappy is null)
+        return Results.BadRequest();
+
+    return Results.Ok(service.Hello((bool)isHappy));
+});
+
+// Run the app
+app.Run();
---
+++ src/Sample.DotNet6.Api/Services/HelloService.cs
@@ -0,0 +1,19 @@
+﻿namespace Sample.DotNet6.Api.Services
+{
+    public interface IHelloService
+    {
+        string Hello(bool isHappy);
+    }
+
+    public class HelloService : IHelloService
+    {
+        public string Hello(bool isHappy)
+        {
+            var hello = $"Hello";
+
+            if (isHappy)
+                return $"{hello}, you seem to be happy today";
+            return hello;
+        }
+    }
+}
